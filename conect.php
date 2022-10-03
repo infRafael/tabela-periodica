6bfa2698a5f745edb222072eb15863d0b38ab506
Rafael Augusto <98691559+infRafael@users.noreply.github.com>
2022-10-02 22:08:39 -0300

    Add files via upload

---
+++ conect.php
@@ -0,0 +1,14 @@
+<?php 
+        $dbHost =  'us-east.connect.psdb.cloud';
+        $dbUsername = '2q6ws37julrb13cg0afd';
+        $dbPassword = 'pscale_pw_mzXiXozHhZwPsBFIXuPq0m6iw24JnHaINgWWrYMgVty';
+        $dbName = 'avaliacoes';
+        
+        $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+        if($conexao->connect_errno){
+                echo 'erro';
+        }else{
+                echo 'conexao efetuada'
+        }
+?>
